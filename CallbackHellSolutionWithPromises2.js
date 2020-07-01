e8fe08a441da586fe5de247959d9e9558ade2d54
Srivatsan <sozhavaramsrivatsan@hotmail.com>
2020-07-02 00:25:48 +0530

    compleated promises

--- CallbackHellSolutionWithPromises2.js
+++ CallbackHellSolutionWithPromises2.js
@@ -4,48 +4,42 @@ B. The entire logic should remain as it is.
 C. You just need to convert the callback chain into promise.
 */
 
-/**
- * Function to get the username from the database
- * @param {*} callback function to be called after getting the username (validateUsername() function)
- */
-const getUsername = (callback) => {
+const getUsername = new Promise((resolve, reject) => {
   console.log('Getting username...');
   // code to get the username
   setTimeout(() => {
     const username = 'srishti';
-    console.log('Username = ' + username);
-    callback(username);
-  }, 3000);
-};
 
-/**
- * Function to validate the username
- * @param {*} username the username which is to be checked as valid or invalid
- * @param {*} callback function to be called if the username is valid
- */
-const validateUsername = (username, callback) => {
-  console.log('Validating username...');
-  // if the username is not undefined or null or empty string, call the getPassword() function
-  if (username !== undefined && username !== null && username !== '') {
-    console.log('Valid Username!');
-    callback();
-  } else {
-    console.log('Invalid Username! Please try again!');
-  }
+    resolve('Username = ' + username);
+  }, 3000);
+});
+
+const validateUsername = (username) => {
+  return new Promise((resolve, reject) => {
+    console.log('Validating username...');
+    // if the username is not undefined or null or empty string, call the getPassword() function
+    if (username !== undefined && username !== null && username !== '') {
+      resolve('Valid Username!');
+    } else {
+      reject('Invalid Username! Please try again!');
+    }
+  });
 };
 
 /**
  * Function to get the password from the database
  * @param {*} callback function to be called after getting the password (validatePassword() function)
  */
-const getPassword = (callback) => {
+const getPassword = () => {
   console.log('Getting password...');
   // code to get the password
-  setTimeout(() => {
-    const password = 'upgrad';
-    console.log('Password = ' + password);
-    callback(password);
-  }, 3000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const password = 'upgrad';
+
+      resolve('Password = ' + password);
+    }, 3000);
+  });
 };
 
 /**
@@ -53,15 +47,16 @@ const getPassword = (callback) => {
  * @param {*} password the password which is to be checked as valid or invalid
  * @param {*} callback function to be called if the password is valid
  */
-const validatePassword = (password, callback) => {
-  console.log('Validating password...');
-  // if the password is not undefined or null or empty string, call the done() function
-  if (password !== undefined || password !== null || password !== '') {
-    console.log('Valid Password!');
-    callback();
-  } else {
-    console.log('Invalid Password! Please try again!');
-  }
+const validatePassword = (password) => {
+  return new Promise((resolve, reject) => {
+    console.log('Validating password...');
+    // if the password is not undefined or null or empty string, call the done() function
+    if (password !== undefined || password !== null || password !== '') {
+      resolve('Valid Password!');
+    } else {
+      resolve('Invalid Password! Please try again!');
+    }
+  });
 };
 
 /**
@@ -76,15 +71,42 @@ const done = () => {
  * CALLBACK HELL
  */
 const login = () => {
-  getUsername((username) => {
-    validateUsername(username, () => {
-      getPassword((password) => {
-        validatePassword(password, () => {
-          done();
-        });
-      });
-    });
-  });
+  getUsername
+    .then(
+      (username) => {
+        console.log(username);
+        return validateUsername(username);
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+    .then(
+      (validation) => {
+        console.log(validation);
+        return getPassword();
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+    .then(
+      (password) => {
+        console.log(password);
+        return validatePassword(password);
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+    .then(
+      (validation) => {
+        console.log(validation);
+        done();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
 };
-
 login();
