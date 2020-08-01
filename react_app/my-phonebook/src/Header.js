37b6f693bd0b2ddd09cc741828ed357341638c2a
Srivatsan <sozhavaramsrivatsan@hotmail.com>
2020-08-01 19:16:26 +0530

    added inline styling

--- react_app/my-phonebook/src/Header.js
+++ react_app/my-phonebook/src/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
 // import React, { Component } from 'React';
 // function component
+let headerStyle= {
+  textAlign:'center',
+  padding:20,
+  background:'#000',
+  color:'#fff',
+  textTransform:'uppercase'
+};
 const Header = function () {
-  return <div className='header'>Phone Directory</div>;
+  return <div style={headerStyle}>Phone Directory</div>;
 };
 
 // class component
