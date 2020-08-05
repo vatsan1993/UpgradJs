e5a5ad1d3e18fba9fd346a1fdea7d1e0715e6686
Srivatsan <sozhavaramsrivatsan@hotmail.com>
2020-08-05 10:27:43 +0530

    Added state to App component and converted the component to class component.

--- react_app/my-phonebook/src/AddSubscriber.js
+++ react_app/my-phonebook/src/AddSubscriber.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import Header from './Header';
 import './AddSubscriber.css';
 class AddSubscriber extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 0,
+      name: '',
+      phone: '',
+    };
+  }
+
+  inputChangeHandler = (e) => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+    console.log(this.state);
+  };
+
   render() {
+    const { name, phone } = this.state;
     return (
       <div>
         <Header heading='Add Subscriber' />
@@ -13,14 +30,24 @@ class AddSubscriber extends Component {
               Name:
             </label>
             <br />
-            <input type='text' className='input-control' name='name' />
+            <input
+              type='text'
+              className='input-control'
+              name='name'
+              onChange={this.inputChangeHandler}
+            />
             <br />
             <br />
             <label htmlFor='phone' className='label-control'>
               Phone:
             </label>
             <br />
-            <input type='text' className='input-control' name='phone' />
+            <input
+              type='text'
+              className='input-control'
+              name='phone'
+              onChange={this.inputChangeHandler}
+            />
             <br />
             <br />
             <div className='subscriber-info-container'>
@@ -29,9 +56,9 @@ class AddSubscriber extends Component {
                 Subscriber to be added:{' '}
               </span>
               <br />
-              <span className='subscriber-info'>Name:</span>
+              <span className='subscriber-info'>Name:{name}</span>
               <br />
-              <span className='subscriber-info'>Phone:</span>
+              <span className='subscriber-info'>Phone:{phone}</span>
               <br />
             </div>
             <button type='submit' className='custom-btn add-btn'>
--- react_app/my-phonebook/src/App.js
+++ react_app/my-phonebook/src/App.js
@@ -1,47 +1,42 @@
-import React from 'react';
-import Header from './Header';
+import React, { Component } from 'react';
+import Header from './Header.js';
 import './App.css';
 
-import { Fragment } from 'react';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: [],
+    };
+  }
 
-let deleteHandler = (msg) => {
-  alert(msg);
-};
+  render() {
+    return (
+      <div>
+        <Header heading='Phone Directory' />
+        <div className='component-body-container'>
+          <button className='custom-btn add-btn'>Add</button>
 
-const subscribers = [
-  { id: 1, name: 'Srivatsan', phone: '9110301748' },
-  { id: 2, name: 'Sridharan', phone: '8886010444' },
-];
-function App() {
-  return (
-    <div>
-      {/* Addeda prop */}
-      <Header heading='Phone Directory' />
-      <div className='component-body-container'>
-        <button className='custom-btn add-btn'>Add</button>
-        <div className='grid-container heading-container'>
-          <span className='grid-item name-heading'>Name</span>
-          <span className='grid-item phone-heading'>Phone</span>
+          <div className='grid-container heading-container'>
+            <span className='grid-item name-heading'>Name</span>
+            <span className='grid-item phone-heading'>Phone</span>
+          </div>
+
+          {this.state.subscribersListToShow.map((sub) => {
+            return (
+              <div key={sub.id} className='grid-container'>
+                <span className='grid-item'>{sub.name}</span>
+                <span className='grid-item'>{sub.phone}</span>
+                <span className='grid-item action-btn-container'>
+                  <button className='custom-btn delete-btn'>Delete</button>
+                </span>
+              </div>
+            );
+          })}
         </div>
-        {subscribers.map((sub) => {
-          return (
-            <div key={sub.id} className='grid-container'>
-              <span className='grid-item '>{sub.name}</span>
-              <span className='grid-item '>{sub.phone}</span>
-              <span className='grid-item action-btn-container'>
-                <button
-                  className='custom-btn delete-btn'
-                  onClick={deleteHandler.bind(this, 'Delete Clicked')}
-                >
-                  DELETE
-                </button>
-              </span>
-            </div>
-          );
-        })}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
